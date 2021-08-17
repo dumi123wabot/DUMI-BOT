a25796f1f281cc518f5bfb644ab81ff5749c53d0
dumi123wabot <88726327+dumi123wabot@users.noreply.github.com>
2021-08-17 19:57:29 +0530

    Create downloader.js

---
+++ downloader.js
@@ -0,0 +1,81 @@
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const got = require('got');
+const fs = require('fs');
+const axios = require('axios');
+const { errorMessage, infoMessage } = require('../helpers');
+const IG_DESC = "වීඩියව බාගත කිරීම"
+const NEED_WORD = "ලින්ක් එකක් යෙදිය යුතුමයි"
+const FBDESC = "මුහුණුපොතින් වීඩියෝව බාගතවෙමින් පවත්..."
+const LOADING = "බාගත වෙමින් පවති..."
+const NOT_FOUNDFB = "වීඩියෝව හමු වූයේ නැත"
+const CAPTION = "Caption"
+
+Asena.addCommand({ pattern: 'ig ?(.*)', fromMe: false, desc: IG_DESC}, async (message, match) => {
+
+    const userName = match[1]
+
+    if (!userName) return await message.sendMessage(errorMessage(NEED_WORD))
+
+    await message.sendMessage(infoMessage(" DUMI BOT Downloading the Post..."))
+
+    await axios
+      .get(`https://api-anoncybfakeplayer.herokuapp.com/igdown?url=${userName}`)
+      .then(async (response) => {
+        const {
+          url,
+          type,
+        } = response.data.result[0]
+
+        const profileBuffer = await axios.get(url, {responseType: 'arraybuffer'})
+
+        const msg = `${type}`
+
+	 if (msg === 'image') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
+          caption: "Made By DUMI BOT"
+        })}
+		 	 
+	if (msg === 'video') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
+          caption: "Made By DUMI BOT"
+        })}
+	
+        
+      })
+      .catch(
+        async (err) => await message.sendMessage(errorMessage("Invaild Link, Please Enter a Vaild Instagram Link")),
+      )
+  },
+)
+
+
+
+
+Asena.addCommand({ pattern: 'fb ?(.*)', fromMe: false, desc: FBDESC }, async (message, match) => {
+
+    const userName = match[1]
+
+    if (!userName) return await message.sendMessage(errorMessage(NEED_WORD))
+
+    await message.sendMessage(infoMessage(LOADING))
+
+    await axios
+      .get(`https://videfikri.com/api/fbdl/?urlfb=${userName}`)
+      .then(async (response) => {
+        const {
+          url,
+          judul,
+        } = response.data.result
+
+        const profileBuffer = await axios.get(url, {responseType: 'arraybuffer'})
+
+        const msg = `*${CAPTION}*: ${judul}`
+
+        await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
+          caption: "Made By DUMI BOT"
+        })
+      })
+      .catch(
+        async (err) => await message.sendMessage(errorMessage(NOT_FOUNDFB )),
+      )
+  },
+)
