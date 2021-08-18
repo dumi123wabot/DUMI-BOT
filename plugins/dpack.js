59f00609c575762d118489ea8f94088cd88f7c13
dumi123wabot <88726327+dumi123wabot@users.noreply.github.com>
2021-08-18 19:28:50 +0530

    Create dpack.js

---
+++ plugins/dpack.js
@@ -0,0 +1,45 @@
+/* # Exclusively from D-GANG
+
+*/
+
+const dgang = require( ../events );
+
+const { MessageType, MessageOptions, Mimetype } = require( @adiwajshing/baileys );
+
+const fs = require( fs );
+
+const axios = require( axios );
+
+const Config = require( ../config );
+
+const need = "some command are not working it s not our false.it is logo site errors.\n🗡🗡"
+
+if (Config.WORKTYPE ==  private ) {
+
+    RAVANA.addCommand({ pattern:  dpack ?(.dpack*) , fromMe: true,dontAddCommandList: true }, (async (message, match) => {
+
+        if (match[1] ===   ) return await message.sendMessage(need);
+
+        var ttinullimage = await axios.get(`https://api.xteam.xyz/api/logobp?apikey=Db2251ba8836d521&text=${encodeURIComponent(match[1])}`, { responseType:  arraybuffer  })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.MADE BY D-GANG})
+
+    }));
+
+}
+
+else if (Config.WORKTYPE ==  public ) {
+
+    RAVANA.addCommand({ pattern:  dpack ?(.dpack*) , fromMe: false,dontAddCommandList: true}, (async (message, match) => {
+
+        if (match[1] ===   ) return await message.sendMessage(need);
+
+        var ttinullimage = await axios.get(`https://api.xteam.xyz/api/logobp?apikey=Db2251ba8836d521&text=${encodeURIComponent(match[1])}`, { responseType:  arraybuffer  })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.MADE BY D-GANG})
+
+    }));
+
+    
+
+}
