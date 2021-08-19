d49f63cb31014587f990a1a7f2d7636213cd557b
dumi123wabot <88726327+dumi123wabot@users.noreply.github.com>
2021-08-19 12:02:31 +0530

    Update system_stats.js

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -1,14 +1,18 @@
-/* Copyright (C) 2020 TOXIC DEVIL
+/* Copyright (C) 2020 Yusuf Usta.
 
-CODDED BY TOXIC DEVIL
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+WhatsAsena - Yusuf Usta
+Developer & Co-Founder - Phaticusthiccy
 */
 
 const Asena = require('../events');
-const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const {MessageType} = require('@adiwajshing/baileys');
 const {spawnSync} = require('child_process');
 const Config = require('../config');
 const chalk = require('chalk');
-const axios = require('axios');
+const Axios = require('axios');
 
 const Language = require('../language');
 const Lang = Language.getString('system_stats');
@@ -18,31 +22,30 @@ if (Config.WORKTYPE == 'private') {
 
     Asena.addCommand({pattern: 'alive', fromMe: true, desc: Lang.ALIVE_DESC}, (async (message, match) => {
 
-        if (Config.ALIVEMSG == '/n හායි මම දැන් ඔන්ලයින්.🥀/n https://chat.whatsapp.com/La5MMeWqcLK0Msq50LvnqX/n join this') {
-            
-            var image = await axios.get (Config.ALIVE_LOGO, {responseType: 'arraybuffer'})
-       
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: config.©𝐃𝐔𝐌𝐈𝐁𝐎𝐓})
+        if (Config.ALIVEMSG == 'default') {
+            await message.client.sendMessage(message.jid,'```Tanrı Türk\'ü Korusun. 🐺 Asena Hizmetinde!```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Telegram Group:* https://t.me/AsenaSupport\n*Telegram Channel:* https://t.me/asenaremaster\n*Plugin Channel:* ' + Config.CHANNEL , MessageType.text);
+        }
+        else {
+            const pow = '*Powered by WhatsAsena*'
+            const payload = Config.ALIVEMSG
+            const status = await message.client.getStatus()
+            const ppUrl = await message.client.getProfilePicture() 
+            const resim = await Axios.get(ppUrl, {responseType: 'arraybuffer'})
 
-    }
-    else {
-            
-            var image = await axios.get (Config.ALIVE_LOGO, {responseType: 'arraybuffer'})
-       
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: Config.©𝐃𝐔𝐌𝐈𝐁𝐎𝐓})
-     }
+            if (!payload.includes('{pp}')) {
+                await message.client.sendMessage(message.jid,payload.replace('{version}', Config.VERSION).replace('{info}', `${status.status}`).replace('{plugin}', Config.CHANNEL) + '\n' + pow, MessageType.text);
+            }
+            else if (payload.includes('{pp}')) {
+                await message.sendMessage(Buffer(resim.data), MessageType.image, { caption: payload.replace('{version}', Config.VERSION).replace('{pp}', '').replace('{info}', `${status.status}`).replace('{plugin}', Config.CHANNEL) + '\n' + pow });
+            }
+        }
     }));
 
     Asena.addCommand({pattern: 'sysd', fromMe: true, desc: Lang.SYSD_DESC}, (async (message, match) => {
 
-        if (message.jid === '905524317852-1612300121@g.us') {
-
-            return;
-        }
-
         const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
         await message.sendMessage(
-            '' + child + '', MessageType.text
+            '```' + child + '```', MessageType.text
         );
     }));
 }
@@ -50,31 +53,30 @@ else if (Config.WORKTYPE == 'public') {
 
     Asena.addCommand({pattern: 'alive', fromMe: false, desc: Lang.ALIVE_DESC}, (async (message, match) => {
 
-        if (Config.ALIVEMSG == 'හායි මම දැන් ඔන්ලයින්.🥀https://chat.whatsapp.com/La5MMeWqcLK0Msq50LvnqX _join this ) {
-            
-            var image = await axios.get (Config.ALIVE_LOGO, {responseType: 'arraybuffer'})
-       
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: config.©𝐃𝐔𝐌𝐈𝐁𝐎𝐓})
+        if (Config.ALIVEMSG == 'default') {
+            await message.client.sendMessage(message.jid,'```හායි මම දැන් ඔන්ලයින්.🥀 !```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*WHATSAPP Group:*https://chat.whatsapp.com/La5MMeWqcLK0Msq50LvnqX*whatsapp plugins group:* https://chat.whatsapp.com/La5MMeWqcLK0Msq50LvnqX\n*:* ' + Config.CHANNEL, MessageType.text);
+        }
+        else {
+            const pow = '*Powered by WhatsAsena*'
+            const payload = Config.ALIVEMSG
+            const status = await message.client.getStatus()
+            const ppUrl = await message.client.getProfilePicture() 
+            const resim = await Axios.get(ppUrl, {responseType: 'arraybuffer'})
 
-    }
-    else {
-            
-            var image = await axios.get (Config.ALIVE_LOGO, {responseType: 'arraybuffer'})
-       
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: config.©𝐃𝐔𝐌𝐈𝐁𝐎𝐓})
-     }
+            if (!payload.includes('{pp}')) {
+                await message.client.sendMessage(message.jid,payload.replace('{version}', Config.VERSION).replace('{info}', `${status.status}`).replace('{plugin}', Config.CHANNEL) + '\n' + pow, MessageType.text);
+            }
+            else if (payload.includes('{pp}')) {
+                await message.sendMessage(Buffer(resim.data), MessageType.image, { caption: payload.replace('{version}', Config.VERSION).replace('{pp}', '').replace('{info}', `${status.status}`).replace('{plugin}', Config.CHANNEL) + '\n' + pow });
+            }
+        }
     }));
 
     Asena.addCommand({pattern: 'sysd', fromMe: false, desc: Lang.SYSD_DESC}, (async (message, match) => {
 
-        if (message.jid === '905524317852-1612300121@g.us') {
-
-            return;
-        }
-
         const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
         await message.sendMessage(
-            '' + child + '', MessageType.text
+            '```' + child + '```', MessageType.text
         );
     }));
 }
